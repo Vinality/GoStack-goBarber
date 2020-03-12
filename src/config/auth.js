c7e763038ad2f88b97d8d6db3ef4ebc0ceb40e5b
Vinicius Crepschi <vinicius.crepschi@rocky.ag>
2020-03-12 11:50:51 -0300

    Module 2 GoBarber

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'b0bb2ec556f0b4647cc1d9cdd2c7f956',
+  expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,16 @@
 import { Router } from 'express';
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Diegod',
-    email: 'daigo@parry.com',
-    password_hash: '123213123',
-  });
+routes.post('/sessions', SessionController.store);
+routes.post('/users', UserController.store);
 
-  return res.json(user);
-});
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
 
 export default routes;
